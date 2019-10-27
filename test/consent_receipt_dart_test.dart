504c40ca6642315045e9acedf346240c874d1c9b
Paul Mundt <paul.mundt@adaptant.io>
2019-10-28 00:05:00 +0100

    Add some basic tests with a minimal CR definition

--- test/consent_receipt_dart_test.dart
+++ test/consent_receipt_dart_test.dart
@@ -1,16 +1,59 @@
 import 'package:consent_receipt_dart/consent_receipt_dart.dart';
+import 'package:consent_receipt_dart/src/data_categories.dart';
+import 'package:validators/validators.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('A group of tests', () {
-    Awesome awesome;
+    ConsentReceiptGenerator gen;
+    ConsentReceipt cr;
 
     setUp(() {
-      awesome = Awesome();
+      gen = ConsentReceiptGenerator(
+        countryCode: 'US',
+        privacyPolicy: 'https://www.example.com/privacy',
+        piiControllers: [
+          DataController(
+            address: PostalAddress(
+              addressCountry: 'US',
+              postalCode: '00000',
+              addressLocality: 'N/A',
+              streetAddress: 'N/A',
+            ),
+            contact: 'Jon Doe',
+            email: 'example@example.com',
+            phone: '00-000-000-0000',
+            piiController: 'DataController, Inc.'
+          ),
+        ],
+      );
+
+      cr = gen.generateConsentReceipt(
+          collectionMethod: 'N/A',
+          subjectId: 'example@example.com',
+          services: [
+            Service(
+              service: "test service",
+              purposes: [
+                Purpose(
+                  piiCategory: [
+                    DataCategory.AssetData,
+                  ],
+                  purposeCategory: PurposeSpecification.CoreFunction,
+                  termination: 'At the end of this test run',
+                ),
+              ],
+            ),
+          ],
+      );
     });
 
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
+    test('Sensitive setting should be false', () {
+      expect(cr.sensitive, false);
+    });
+
+    test('ReceiptID should be a valid UUID', () {
+      expect(isUUID(cr.consentReceiptID), true);
     });
   });
 }
